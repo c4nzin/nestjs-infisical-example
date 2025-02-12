658b1117ba3738ec9e61ac2c03538746dee165a0
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2025-02-12 22:16:07 +0300

    feat: add async register

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,33 @@ import { AppController } from "./app.controller";
 import * as dotenvx from "@dotenvx/dotenvx";
 import { AppService } from "./app.service";
 import { InfisicalModule } from "nestjs-infisical-sdk";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 dotenvx.config({});
 
 @Module({
   imports: [
-    InfisicalModule.register({
-      clientId: process.env.INFISICAL_CLIENT_ID,
-      clientSecret: process.env.INFISICAL_CLIENT_SECRET,
-      projectId: process.env.INFISICAL_PROJECT_ID,
-      environment: "dev",
-      watchEnvFile: true,
-      injectIntoProcessEnv: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    // InfisicalModule.register({ If you want to use not with async just uncomment it.
+    //   clientId: process.env.INFISICAL_CLIENT_ID,
+    //   clientSecret: process.env.INFISICAL_CLIENT_SECRET,
+    //   projectId: process.env.INFISICAL_PROJECT_ID,
+    //   environment: "dev",
+    //   watchEnvFile: true,
+    //   injectIntoProcessEnv: true,
+    // )},
+    InfisicalModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        apiKey: configService.get<string>("INFISICAL_API_KEY"),
+        apiSecret: configService.get<string>("INFISICAL_API_SECRET"),
+        injectIntoProcessEnv: true,
+        projectId: configService.get<string>("INFISICAL_PROJECT_ID"),
+        clientId: configService.get<string>("INFISICAL_CLIENT_ID"),
+        clientSecret: configService.get<string>("INFISICAL_CLIENT_SECRET"),
+        environment: "dev",
+        watchEnvFile: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
